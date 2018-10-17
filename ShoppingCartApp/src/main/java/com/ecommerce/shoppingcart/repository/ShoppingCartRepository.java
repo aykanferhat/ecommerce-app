5823bc1d5f2073ada6e74922309e1bc1686489b4
Ferhat Aykan <ferhat.aykan@sahabt.com>
2018-10-18 01:45:18 +0300

    deleted shoppingcart name

--- ShoppingCartApp/src/main/java/com/ecommerce/shoppingcart/repository/ShoppingCartRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.shoppingcart.repository;
-
-import com.ecommerce.core.entity.ShoppingCartEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCartEntity, Long> {
-}
